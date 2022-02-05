9d7ba9c4d29a38c66647148ad8c22f5c5590ea31
Gene Duquette <bean@theduquettes.net>
2022-02-04 17:20:03 -0800

    Experiment Spring R2DBC with H2 DB

--- src/main/java/com/duquette/experiment/spring/react/ReactApplication.java
+++ src/main/java/com/duquette/experiment/spring/react/ReactApplication.java
@@ -1,13 +1,78 @@
 package com.duquette.experiment.spring.react;
 
+import io.r2dbc.spi.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+import com.duquette.experiment.spring.react.entity.Customer;
+import com.duquette.experiment.spring.react.repository.CustomerRepository;
 
 @SpringBootApplication
 public class ReactApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(ReactApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactApplication.class, args);
+    }
+
+	// From https://spring.io/guides/gs/accessing-data-r2dbc/ this isn't needed because spring initializes it from the resource.
+    // @Bean
+    // ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+    //     ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+    //     initializer.setConnectionFactory(connectionFactory);
+    //     initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+
+    //     return initializer;
+    // }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            // save a few customers
+            repository.saveAll(Arrays.asList(new Customer("Jack", "Bauer"),
+                new Customer("Chloe", "O'Brian"),
+                new Customer("Kim", "Bauer"),
+                new Customer("David", "Palmer"),
+                new Customer("Michelle", "Dessler")))
+                .blockLast(Duration.ofSeconds(10));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().doOnNext(customer -> {
+                log.info(customer.toString());
+            }).blockLast(Duration.ofSeconds(10));
+
+            log.info("");
+
+            // fetch an individual customer by ID
+			repository.findById(1L).doOnNext(customer -> {
+				log.info("Customer found with findById(1L):");
+				log.info("--------------------------------");
+				log.info(customer.toString());
+				log.info("");
+			}).block(Duration.ofSeconds(10));
+
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").doOnNext(bauer -> {
+                log.info(bauer.toString());
+            }).blockLast(Duration.ofSeconds(10));;
+            log.info("");
+        };
+    }
 
 }
---
+++ src/main/java/com/duquette/experiment/spring/react/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.duquette.experiment.spring.react.repository;
+
+import com.duquette.experiment.spring.react.entity.Customer;
+
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import reactor.core.publisher.Flux;
+
+public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
+
+    @Query("SELECT * FROM customer WHERE last_name = :lastname")
+    Flux<Customer> findByLastName(String lastName);
+}
